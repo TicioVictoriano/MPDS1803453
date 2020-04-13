05c532ec977ea11b4bc6fbdf8e4ff324b2a858ad
TicioVictoriano <tvictoriano16@alustudent.com>
2020-04-13 23:34:03 +0100

    Update Common.java

--- app/src/main/java/gcu/ac/uk/coursework_s1803453/Common/Common.java
+++ app/src/main/java/gcu/ac/uk/coursework_s1803453/Common/Common.java
@@ -6,7 +6,7 @@
 package gcu.ac.uk.coursework_s1803453.Common;
 
 public class Common{
-    public static double mau_lat = 20.3484;
+    public static double mau_lat = -20.3484;
     public static double mau_long = 57.5522;
     public static String URL = "https://quakes.bgs.ac.uk/feeds/WorldSeismology.xml";
 }
